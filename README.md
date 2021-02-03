bfdfa90d9b89e32b9dbb5dfd3c3ec16e1f112b4d
ahviplc <ahlc@sina.cn>
2021-02-03 17:50:09 +0800

    make electron-builder better.

--- README.md
+++ README.md
@@ -1,5 +1,27 @@
 # my-electron-vue3-app
 
+> a demo of the electron-vue3 by LC.
+
+> electron与vue-cli3.0脚手架做了很好的融合.
+
+> 按照下面网址这个教程-新建了vue3和electron整合项目 my-electron-vue3-app
+
+> electron-vue 安装环境、构建+打包（mac和windows）这一篇就够了_阿毛sky的博客-CSDN博客
+  https://blog.csdn.net/qq_32244819/article/details/110820687
+
+# 技术栈
+```markdown
+此my-electron-vue-app 使用技术栈以及其版本 比较新的版本 也就是按照上面教程初始化项目所用的版本
+> Vue.js:3.0.5
+
+> Electron:11.2.2
+
+> Node:12.18.3
+
+```
+
+# run it.
+> 很多命令npm run * 具体请看package.json的"scripts".
 ## Project setup
 ```
 npm install
@@ -8,6 +30,20 @@ npm install
 ### Compiles and hot-reloads for development
 ```
 npm run serve
+
+注意:
+在my-electron-vue3-app\vue.config.js中修改web端口
+ devServer: {
+        port: 6886
+    }
+
+执行成功输出:
+  App running at:
+  - Local:   http://localhost:6886/
+  - Network: http://192.168.0.16:6886/
+
+  Note that the development build is not optimized.
+  To create a production build, run npm run build.
 ```
 
 ### Compiles and minifies for production
@@ -20,5 +56,63 @@ npm run build
 npm run lint
 ```
 
+### Compiles the electron and hot-reloads for development
+```
+npm run electron:serve
+```
+
+### Compiles the electron and minifies for production
+```
+npm run electron:build
+
+# 打包electron
+这里是使用electron-builder进行打包
+编译为win64下的安装包
+执行命令:
+> npm run electron:build
+或者纯打安装包 不好使 不可用
+> npm run app:win64
+
+如需编译为win32下的安装包
+放开下面代码注释即可
+win32 -> vue.config.js:36 
+或者纯打安装包 不好使 不可用
+> npm run app:win32
+
+如需编译mac下的安装包
+"app:mac": "electron-builder --mac"
+直接执行命令: 不好使 不可用
+> npm run app:mac
+
+备注:
+"app:win32": "electron-builder --win --ia32",
+"app:win64": "electron-builder --win --x64",
+"app:mac": "electron-builder --mac"
+在此项目都不好使 不可用.
+```
+
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+# About me
+
+```
+https://github.com/ahviplc
+
+https://github.com/ahviplc/my-electron-vue3-app
+
+https://github.com/ahviplc/my-electron-vue3-app.git
+```
+
+---
+> 官方版 electron demo 请去下面链接
+
+> ahviplc/my-electron-app: my-electron-app demo by LC.
+https://github.com/ahviplc/my-electron-app
+
+---
+> 基于 vue (基本上是它听起来的样子) 来构造 electron 应用程序的样板代码 electron-vue demo 请去下面链接 技术栈版本低一点
+
+> ahviplc/my-electron-vue-app: a demo of the electron-vue by LC.
+https://github.com/ahviplc/my-electron-vue-app
+
--- src/background.js
+++ src/background.js
@@ -1,80 +1,104 @@
 'use strict'
 
-import { app, protocol, BrowserWindow } from 'electron'
-import { createProtocol } from 'vue-cli-plugin-electron-builder/lib'
-import installExtension, { VUEJS_DEVTOOLS } from 'electron-devtools-installer'
+import {app, protocol, BrowserWindow} from 'electron'
+import {createProtocol} from 'vue-cli-plugin-electron-builder/lib'
+// import installExtension, {VUEJS_DEVTOOLS} from 'electron-devtools-installer'
+
 const isDevelopment = process.env.NODE_ENV !== 'production'
 
 // Scheme must be registered before the app is ready
 protocol.registerSchemesAsPrivileged([
-  { scheme: 'app', privileges: { secure: true, standard: true } }
+    {scheme: 'app', privileges: {secure: true, standard: true}}
 ])
 
 async function createWindow() {
-  // Create the browser window.
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      
-      // Use pluginOptions.nodeIntegration, leave this alone
-      // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
-      nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION
-    }
-  })
+    // Create the browser window.
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            // Use pluginOptions.nodeIntegration, leave this alone
+            // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
+            nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION,
+            webSecurity: false,// 这个是为了解决编译打包之后安装到自定义文件夹下无法正常运行 报错 Not allowed to load local resource: file:///D:/#diyappelec\my-electron-vue-app\resources\app.asar\dist\electron/index.html
+            // 这里Electron版本是11.2.2,需下面配置为true 才可以使用remote
+            // 在electron 10.0.0之后，remote模块默认关闭
+            // 必须手动设置webPreferences中的enableRemoteModule为true之后才能使用
+            // 这里待验证 目前无法使用
+            enableRemoteModule: true
+        }
+    })
 
-  if (process.env.WEBPACK_DEV_SERVER_URL) {
-    // Load the url of the dev server if in development mode
-    await win.loadURL(process.env.WEBPACK_DEV_SERVER_URL)
-    if (!process.env.IS_TEST) win.webContents.openDevTools()
-  } else {
-    createProtocol('app')
-    // Load the index.html when not in development
-    win.loadURL('app://./index.html')
-  }
+    if (process.env.WEBPACK_DEV_SERVER_URL) {
+        // Load the url of the dev server if in development mode
+        await win.loadURL(process.env.WEBPACK_DEV_SERVER_URL)
+        if (!process.env.IS_TEST) win.webContents.openDevTools()
+    } else {
+        createProtocol('app')
+        // Load the index.html when not in development
+        win.loadURL('app://./index.html')
+    }
 }
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+    // On macOS it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
 })
 
 app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
 })
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.on('ready', async () => {
-  if (isDevelopment && !process.env.IS_TEST) {
-    // Install Vue Devtools
-    try {
-      await installExtension(VUEJS_DEVTOOLS)
-    } catch (e) {
-      console.error('Vue Devtools failed to install:', e.toString())
-    }
-  }
-  createWindow()
+    // 这里先不安装Vue Devtools了 因网络问题 不好安装 导致electron执行缓慢
+    // if (isDevelopment && !process.env.IS_TEST) {
+    //   // Install Vue Devtools
+    //   try {
+    //     await installExtension(VUEJS_DEVTOOLS)
+    //   } catch (e) {
+    //     console.error('Vue Devtools failed to install:', e.toString())
+    //   }
+    // }
+
+    // 这里使用await console输出一下
+    await awaitConsole()
+
+    // 其他一些输出
+    console.log('background.js app.on ready process.env.IS_TEST -> ', process.env.IS_TEST)
+    console.log('background.js app.on ready process.env.WEBPACK_DEV_SERVER_URL -> ', process.env.WEBPACK_DEV_SERVER_URL)
+    console.log('background.js app.on ready process.env.ELECTRON_NODE_INTEGRATION -> ', process.env.ELECTRON_NODE_INTEGRATION)
+    console.log('background.js app.on ready process.versions.node -> ', process.versions.node)
+    console.log('background.js app.on ready process.versions.electron -> ', process.versions.electron)
+    console.log('background.js app.on ready vue -> ', require('vue/package.json').version)
+    // 继续原来逻辑
+    createWindow()
 })
 
 // Exit cleanly on request from parent process in development mode.
 if (isDevelopment) {
-  if (process.platform === 'win32') {
-    process.on('message', (data) => {
-      if (data === 'graceful-exit') {
-        app.quit()
-      }
-    })
-  } else {
-    process.on('SIGTERM', () => {
-      app.quit()
-    })
-  }
+    if (process.platform === 'win32') {
+        process.on('message', (data) => {
+            if (data === 'graceful-exit') {
+                app.quit()
+            }
+        })
+    } else {
+        process.on('SIGTERM', () => {
+            app.quit()
+        })
+    }
+}
+
+// just console.log()
+function awaitConsole() {
+    console.log('background.js app.on ready async await 输出一下')
 }
